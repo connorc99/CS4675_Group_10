24b5acc8f320161083fcda3c83ccc3f7852aacd1
Zach McGee <zach@upperlevelcrm.com>
2022-03-26 20:13:13 -0400

    update multiple_spiders script to use CrawlerRunner

--- postscrape/postscrape/spiders/multiple_spiders.py
+++ postscrape/postscrape/spiders/multiple_spiders.py
@@ -21,15 +21,20 @@ reactor.run() # the script will block here until the last crawl call is finished
 '''
 
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from post_spyder import SuperSpider 
 
-
+configure_logging();
 settings = get_project_settings()
-process = CrawlerProcess(settings)
-process.crawl(SuperSpider, allowed_suffix= ["a", "b", "c"], name="first")
-process.crawl(SuperSpider, allowed_suffix = ["d", "e", "f"], name="second")
+runner = CrawlerRunner(settings)
+runner.crawl(SuperSpider, allowed_suffix= ["a", "b", "c"], name="first")
+runner.crawl(SuperSpider, allowed_suffix = ["d", "e", "f"], name="second")
 #ideally work on persistance of scraped files
 
-process.start() # the script will block here until all crawling jobs are finished
+d = runner.join()
+d.addBoth(lambda _: reactor.stop())
+
+reactor.run() # the script will block here until all crawling jobs are finished
