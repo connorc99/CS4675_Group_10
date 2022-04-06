b2991f506134a0f594d3aa126bb721deaadaa883
connorc99 <connorcolecc@gmail.com>
2022-04-06 18:50:58 -0400

    Output for stats is robust in sqlite for analysis. Will try to tweak scraping settings, then will be good for analysis

--- postscrape/postscrape/spiders/multiple_spiders.py
+++ postscrape/postscrape/spiders/multiple_spiders.py
@@ -27,11 +27,59 @@ from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from post_spyder import SuperSpider 
 
+import sqlite3
+from sqlite3 import Error
+
+import time
+
 configure_logging();
 settings = get_project_settings()
 runner = CrawlerRunner(settings)
-runner.crawl(SuperSpider, allowed_suffix= ["a", "b", "c"], name="first")
-runner.crawl(SuperSpider, allowed_suffix = ["d", "e", "f"], name="second")
+
+subprocesses = 2
+filter_depth = 3
+max_depth = 5
+
+table_name = "subprocesses_{}_filterdepth_{}_maxdepth_{}".format(subprocesses, filter_depth, max_depth) 
+conn = sqlite3.connect(".\\db\\urldatabase.db")
+print(sqlite3.version)
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS {}".format(table_name))
+cur.execute('''
+    CREATE TABLE {} (
+    insertion_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    scraper_name VARCHAR(100),
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount_scraped_rough INT,
+    enqueued INT,
+    dequeued INT,
+    request_count INT,
+    response_count INT,
+    duplicates_filtered_internal INT,
+    duplicates_filtered_from_others INT,
+    offsites_filtered INT,
+    computing_count INT,
+    undergrad_count INT,
+    research_count INT,
+    online_count INT,
+    current_time TIMESTAMP, 
+    subprocesses INT DEFAULT {},
+    filter_depth INT DEFAULT {},
+    max_depth INT DEFAULT {}
+    )
+    
+    '''.format(table_name, subprocesses, filter_depth, max_depth))
+conn.commit()
+conn.close()
+
+
+time.sleep(3)
+
+#scraper_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+# name VARCHAR(255), max_depth INT, filter_depth INT, creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+runner.crawl(SuperSpider, allowed_suffix= ["a", "b", "c"], name="scraper_1", table_name = table_name)
+runner.crawl(SuperSpider, allowed_suffix = ["d", "e", "f"], name="scraper_2",  table_name = table_name)
 #ideally work on persistance of scraped files
 
 d = runner.join()
--- postscrape/postscrape/spiders/post_spyder.py
+++ postscrape/postscrape/spiders/post_spyder.py
@@ -13,6 +13,28 @@ import time
 import sqlite3
 from sqlite3 import Error
 
+'''
+Change these between runs
+filter_depth: when to start checking URLs that have been added for appropriate suffix, we MANUALLY check this w/ suffix list
+max_depth: the deepest inside a suffix the scraper can go, scrapy checks this for us AUTOMATICALLY
+
+Issue: we DO want to scrape an already scraped page if we are on or past filter_depth, but just add the new links
+    -Alternative is we DONT scrape the page
+
+2 Implementations:
+    1. We scrape already scraped pages in each scraper, and only check to see if that specific scraper has scraped that URL yet
+    2. We don't scrape pages already scraped, which will be quicker and discover less pages
+
+We can talk about the tradeoffs in the writeup of each
+
+This file will be doing option 2- we will NOT scrape a page if another scraper has already scraped it
+
+I think this still parses every page, but only scrapes keywords from ones between filter and max depth. 
+We can discuss a limitation in knowing how to implement not having the follow=True workaround
+'''
+filter_depth = 10
+max_depth = filter_depth + 5
+
 class SuperSpider(CrawlSpider):
     name = 'extractor'
     allowed_domains = ['www.cc.gatech.edu']
@@ -25,36 +47,37 @@ class SuperSpider(CrawlSpider):
         "research": [],
         "online": []
     }
-
     custom_settings = {
-        "DEPTH_LIMIT": 10
+        "DEPTH_LIMIT": max_depth 
     }
-
-
     pages_crawled = 0
 
-    def __init__(self, name = "default", allowed_suffix = None, max_depth = 20, filter_depth = 10):
+    '''
+    name: name of the scraper for table
+    allowed_suffix: list of letters we will allow as the suffix
+    '''
+    def __init__(self, name = "default", allowed_suffix = None, table_name = None):
         super(SuperSpider, self).__init__()
         self.name = name
+        self.table_name = table_name
         self.suffix_list = allowed_suffix
         self.max_depth = max_depth
         self.filter_depth = filter_depth
-        '''
-        custom_settings = {
-            "DEPTH_LIMIT": max_depth #not currently working :(
-        }
-        '''
         self.connection = self.create_connection(".\\db\\urldatabase.db")
+        self.duplicates_from_other_scraper = 0
+        self.last_dequeue_value = -9999
+        self.cur = self.connection.cursor()
         try:
-            self.cur = self.connection.cursor()
+            '''
             self.cur.execute("CREATE TABLE IF NOT EXISTS scraper (scraper_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), max_depth INT, filter_depth INT, creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
             self.cur.execute("CREATE TABLE IF NOT EXISTS url (url_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, scraper_id INT NOT NULL, url VARCHAR(255) NOT NULL, creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 
             self.cur.execute("INSERT INTO scraper (name, max_depth, filter_depth) VALUES ('{}', {}, {})".format(self.name, self.max_depth, self.filter_depth));
-
-            self.scraper_id = self.cur.lastrowid;
+            '''
+            self.scraper_id = self.cur.lastrowid
             self.connection.commit()
-            print(self.scraper_id);
+            print(self.scraper_id)
+            
         except Error as e:
             print(e)
             time.sleep(4)
@@ -66,32 +89,26 @@ class SuperSpider(CrawlSpider):
 
         #check and see if already in database, if so do not parse the item
         if self.check_in_db(response.url):
+            self.duplicates_from_other_scraper += 1
             return
 
-        #time.sleep(1) #delete after testing
-
+        #get the letter for suffix
         suffix = response.url.split("edu")[1][0]
 
-        if response.meta['depth'] > self.filter_depth and False and suffix not in self.suffix_list: #once past certain level, must meet prefix
+        if (response.meta['depth'] > self.filter_depth) and (suffix not in self.suffix_list): #once past certain level, must meet prefix
             self.log('filtered out suffix past depth')
             return
-        if self.pages_crawled % 100 == 0:
-            stats = self.crawler.stats.get_stats()
-            with open('test_stats_{}.csv'.format(self.pages_crawled), 'w') as csv_file:
-                writer = csv.writer(csv_file)
-                for key, value in stats.items():
-                    writer.writerow([key, value])
-                for key, value in self.keywords.items():
-                    writer.writerow([key, len(value)])
-                    print([key, len(value)])
-                writer.writerow(["current_time", datetime.datetime.now()])
+        
 
-            with open('keyword_current_output.csv', 'w') as csv_file:
-                writer = csv.writer(csv_file)
-                for key, value in self.keywords.items():
-                    writer.writerow([key, value])
+        #adds persistent stats- should probably convert this to SQL
+        print("stats check!!")
+        print(self.crawler.stats.get_stats())
+        if self.crawler.stats.get_stats()["scheduler/dequeued"] - self.last_dequeue_value > 500:
+            self.last_dequeue_value = self.crawler.stats.get_stats()["scheduler/dequeued"] 
+            self.write_stats()
 
-        self.pages_crawled += 1
+
+
         for keyword in self.keywords:
             if keyword in response.text.lower():
                 self.keywords[keyword].append(response.url)
@@ -101,7 +118,95 @@ class SuperSpider(CrawlSpider):
         #add to persistent memory storage
         self.add_to_db(response.url)
 
+
+
         #print(self.crawler.stats.get_stats())
+
+    '''
+    def write_stats_old(self):
+        print("-------------------\nWriting stats! We are at {}".format(self.pages_crawled))
+        time.sleep(2)
+        try:
+                stats = self.crawler.stats.get_stats()
+                stats_path = "output_stats/{}/{}/".format(self.table_name, self.name)
+                with open('{}/stats_{}.csv'.format(stats_path, self.pages_crawled), 'w') as csv_file:
+                    writer = csv.writer(csv_file)
+                    for key, value in stats.items():
+                        writer.writerow([key, value])
+                    for key, value in self.keywords.items():
+                        writer.writerow([key, len(value)])
+                        print([key, len(value)])
+                    writer.writerow(["current_time", datetime.datetime.now()])
+
+                with open('{}/keword_stats.csv'.format(stats_path), 'w') as csv_file:
+                    writer = csv.writer(csv_file)
+                    for key, value in self.keywords.items():
+                        writer.writerow([key, value])
+                print("Added stats!")
+                time.sleep(2)
+        except Exception as e:
+            print("stats error!")
+            print(e)
+            time.sleep(3)
+    '''
+
+    def write_stats(self):
+        '''
+        - Create the table for this configuration if one does not exist
+        - Drop all old records from the table
+        - So just drop and then create I guess
+
+        - Insert into the table the current scraper name, start time, currently enqueued, currently dequeued, request count, response count, 
+        duplicates in this scraper filtered, DUPES OVERALL FILTERED- NEED TO TRACK, offsite requests filtered, current keyword stats and current time
+
+        '''
+        try:
+            stats = self.crawler.stats.get_stats()
+            print(stats)
+            insert_query = f'''
+            INSERT INTO {self.table_name} 
+            (
+                scraper_name,
+                enqueued,
+                dequeued,
+                request_count,
+                response_count,
+                duplicates_filtered_internal,
+                duplicates_filtered_from_others,
+                offsites_filtered,
+                computing_count,
+                undergrad_count,
+                research_count,
+                online_count,
+                current_time
+            )
+            VALUES
+            (
+                '{self.name}',
+                {stats["scheduler/enqueued"]}, 
+                {stats["scheduler/dequeued"]},
+                {stats["downloader/request_count"]},
+                {stats["downloader/response_count"]},
+                {stats["dupefilter/filtered"]},
+                {self.duplicates_from_other_scraper},
+                {stats["offsite/filtered"]},
+                {len(self.keywords["computing"])},
+                {len(self.keywords["undergraduate"])},
+                {len(self.keywords["research"])},
+                {len(self.keywords["online"])},
+                CURRENT_TIMESTAMP
+            )
+            '''
+            print(insert_query)
+            self.cur.execute(insert_query)
+            self.connection.commit()
+            print("SUCCESS IN INSERTION")
+        except Exception as e:
+            print("ERROR INSERTING: {}".format(e))
+            print("Sleeping for 3...")
+        finally:
+            time.sleep(3)
+
 
     def close(self, spider):
         self.log("KEYWORDS:::::")
@@ -120,13 +225,19 @@ class SuperSpider(CrawlSpider):
                 writer.writerow([key, value])
 
     def check_in_db(self, url):
-        print("checking in database...")
-        # with open("scraped_urls.json", "r") as json_file:
-        #     data = json.load(json_file)
-        #     return url in data["url"]
-        self.cur.execute("select * from url where url='{}'".format(url))
-        output = self.cur.fetchall()
-        print("Checking if already in database- output from DB is {}".format(output))
+        try:
+            print("checking in database...")
+            # with open("scraped_urls.json", "r") as json_file:
+            #     data = json.load(json_file)
+            #     return url in data["url"]
+            self.cur.execute("select * from url where url='{}'".format(url))
+            output = self.cur.fetchall()
+            print("Checking if already in database- output from DB is {}".format(output))
+        except Exception as e:
+            print("Error checking database: {}".format(e))
+            time.sleep(3)
+            return False
+
         return output != []
 
 
